29ea19261ae7ecb8ea68f4e4f3d5ed7ed0d44181
fikrirehan80-max <fikrirehan80@gmail.com>
2026-09-06 15:27:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,14 @@
+import './globals.css'
+
 export const metadata = {
   title: 'Zhirexz Store',
   description: 'Toko Online Zhirexz',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body className="bg-black text-white">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
